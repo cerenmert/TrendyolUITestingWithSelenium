057f1a744b67ed804e8874fd05cbc567645a779c
Ceren Mert <ceren1481@hotmail.com>
2021-03-25 00:22:44 +0300

    product page

---
+++ src/main/java/com/trendyol/selenium/Pages/ProductResultPage.java
@@ -0,0 +1,18 @@
+package com.trendyol.selenium.Pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ProductResultPage extends BasePage{
+
+    public ProductResultPage(WebDriver webDriver) {
+        super(webDriver);
+
+    }
+    public void pageScrool(){
+        WebDriver driver = new ChromeDriver();
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,500)");
+    }
+}
---
+++ src/test/java/com/trendyol/selenium/CategoryTest.java
@@ -0,0 +1,15 @@
+package com.trendyol.selenium;
+
+import com.trendyol.selenium.Pages.HomePage;
+import com.trendyol.selenium.Pages.ProductResultPage;
+import org.testng.annotations.Test;
+
+public class CategoryTest extends  BaseTest{
+
+    @Test
+    public void ChoseCategoryAndClick() throws InterruptedException {
+        HomePage homePage=this.uyeOlAndGoHome();
+        ProductResultPage productResultPage = homePage.choseCategory();
+        productResultPage.pageScrool();
+    }
+}
